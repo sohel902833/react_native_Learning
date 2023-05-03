55bb10d6927401cc8497cd79392f7e3e5e683ba5
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2023-05-03 10:31:23 +0600

    completed all possible components as a button

--- App.tsx
+++ App.tsx
@@ -6,13 +6,23 @@ import {
   TextInput,
   TouchableWithoutFeedback,
   Keyboard,
+  Button,
+  TouchableOpacity,
+  TouchableHighlight,
+  Pressable,
 } from 'react-native';
 
 const App = () => {
   const [name, setName] = useState('asdf');
+  const [submitted, setSubmitted] = useState(false);
   const handleKeyboardHide = () => {
     Keyboard.dismiss();
   };
+
+  const handleSubmit = () => {
+    setSubmitted(prev => !prev);
+  };
+
   return (
     <TouchableWithoutFeedback onPress={handleKeyboardHide}>
       <View style={styles.container}>
@@ -27,7 +37,33 @@ const App = () => {
           maxLength={10}
           secureTextEntry={true}
         />
-        <Text style={styles.text}>Your Name Is: {name}</Text>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.buttonStyle}
+          onPress={handleSubmit}>
+          <Text style={styles.buttonText}>
+            {submitted ? 'Submit' : 'Clear'}
+          </Text>
+        </TouchableOpacity>
+        <TouchableHighlight
+          activeOpacity={0.5}
+          style={styles.buttonStyle}
+          underlayColor="black"
+          onPress={handleSubmit}>
+          <Text style={styles.buttonText}>
+            {submitted ? 'Submit' : 'Clear'}
+          </Text>
+        </TouchableHighlight>
+        <Pressable
+          android_ripple={{color: 'green'}}
+          style={styles.buttonStyle}
+          onPress={handleSubmit}>
+          <Text style={styles.buttonText}>
+            {submitted ? 'Submit' : 'Clear'}
+          </Text>
+        </Pressable>
+        {/* <Button title={submitted ? 'Submit' : 'Clear'} onPress={handleSubmit} /> */}
+        {submitted && <Text style={styles.text}>Your Name Is: {name}</Text>}
       </View>
     </TouchableWithoutFeedback>
   );
@@ -54,5 +90,16 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 20,
     color: 'black',
+    marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: 'blue',
+    fontWeight: 'bold',
+  },
+  buttonStyle: {
+    backgroundColor: 'pink',
+    padding: 10,
+    borderRadius: 10,
   },
 });
